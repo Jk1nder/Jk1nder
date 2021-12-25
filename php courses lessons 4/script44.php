64df92af70591bacf4cfe2bbc7c801e9bea28fb4
Jk1nder <96064507+Jk1nder@users.noreply.github.com>
2021-12-25 22:03:07 +0200

    Update script44.php

--- php courses lessons 4/script44.php
+++ php courses lessons 4/script44.php
@@ -1,42 +1,42 @@
-<html>
-<head>
-    <title>4</title>
-    <meta charset="UTF-8">
-</head>
-<body>
-
-<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-    Enter words but no more than forty characters in the word: <input type="text" name="name"><br>
-    <input type="submit">
-</form>
-<?php
-if (!empty($_POST['name'])) {
-    $f1 = $_POST['name'];
-$f2 = explode(' ',$f1); 
-foreach ($f2 as $key => $value) {
- if (mb_strlen($f2[$key])>40) {
-echo "Please observe the input conditions";
-exit;
-}
+<html> 
+<head> 
+    <title>4</title> 
+    <meta charset="UTF-8"> 
+</head> 
+<body> 
+ 
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post"> 
+    Enter words but no more than forty characters in the word: <input type="text" name="name"><br> 
+    <input type="submit"> 
+</form> 
+<?php 
+if (!empty($_POST['name'])) { 
+    $f1 = $_POST['name']; 
+$f2 = explode(' ',$f1);  
+foreach ($f2 as $key => $value) { 
+ if (mb_strlen($f2[$key])>40) { 
+echo "Please observe the input conditions"; 
+exit; 
 } 
-$strlen = 0; 
-$array = []; 
-$i = 0; 
-foreach($f2 as $value){ 
- $strlen++; 
- $strlen += mb_strlen($value); 
- if($strlen > 40){ 
-  $i++; 
-  $strlen = 0; 
- } 
- $array[$i] .= $value." "; 
+}  
+$strlen = 0;  
+$array = [];  
+$i = 0;  
+foreach($f2 as $value){  
+ $strlen++;  
+ $strlen += mb_strlen($value);  
+ if($strlen > 40){  
+  $i++;  
+  $strlen = mb_strlen($value);  
+ }  
+ $array[$i] .= $value." ";  
+}  
+$result = implode("<br>",$array);  
+print_r($result);  
+echo $newtext;  
+ 
 } 
-$result = implode("<br>",$array); 
-print_r($result); 
-echo $newtext; 
-
-}
-?>
-
-</body>
+?> 
+ 
+</body> 
 </html>
